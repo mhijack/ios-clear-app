380a1223923f12be9f92d2b79f72e6dcd5c763a1
Jianyuan Chen (Jack) <jianyuan.chen12@gmail.com>
2019-03-20 11:22:10 -0700

    data persisted through user defaults

--- ios-to-do/TodoListViewController.swift
+++ ios-to-do/TodoListViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var items = [
-        "MADIOS-1",
-        "MADIOS-2",
-        "MADIOS-3"
-    ]
-
+    let ITEMS = "ITEMS"
+    var items = ["MADIOS-1", "MADIOS-2", "MADIOS-3"]
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+ 
+        if let itemArr = defaults.array(forKey: self.ITEMS) as? [String] {
+            items = itemArr
+        }
         decorateNavigationItem()
     }
     
@@ -48,19 +49,23 @@ class TodoListViewController: UITableViewController {
         return cell;
     }
     
+    // MARK: Editing cell
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             self.items.remove(at: indexPath.row)
-//            tableView.reloadData()
+            //            tableView.reloadData()
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
-
+    
+    // MARK: Customize UIAlertController
     @IBAction func addButtonTapped(_ sender: Any) {
         let alert = UIAlertController(title: "What?", message: "Say something here", preferredStyle: UIAlertController.Style.alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             self.items.append(alert.textFields?.first?.text ?? "")
+            
+            self.defaults.set(self.items, forKey: self.ITEMS)
+            
             self.tableView.reloadData()
         }
         alert.addTextField { (textfield) in
